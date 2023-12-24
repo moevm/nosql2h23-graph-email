302a349db5a7d927d175d1cc3022c367601086e6
youngwow <nikitaboyarkin15@gmail.com>
2023-12-24 17:08:21 +0300

    docker: done

--- server/app/config.py
+++ server/app/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 FLASK_PORT = environ.get('FLASK_PORT', 5000)
-NEO4J_URI = environ.get('NEO4J_URI', "bolt://localhost:7687")
+NEO4J_URI = environ.get('NEO4J_URI', "bolt://neo4j:7687")
 DEBUG = environ.get('DEBUG', 'True') == 'True'
 NEO4J_USER = environ.get('NEO4J_USER', "neo4j")
 NEO4J_PASSWORD = environ.get('NEO4J_PASSWORD', "admin1234")
